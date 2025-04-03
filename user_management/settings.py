b93c662faf5279612ca6880de61ab536d33239c3
Naim Momin <mominnaim11@gmail.com>
2025-04-03 01:29:07 -0500

    Dockerfile uploaded

--- user_management/settings.py
+++ user_management/settings.py
@@ -230,6 +230,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
